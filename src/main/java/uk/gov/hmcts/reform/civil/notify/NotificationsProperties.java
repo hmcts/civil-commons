0217aca759c889f91fb97bf288d160d549f21443
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-08-04 10:55:02 +0100

    CIV-7733 Test commit

--- src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
+++ src/main/java/uk/gov/hmcts/reform/civil/notify/NotificationsProperties.java
@@ -328,7 +328,6 @@ public class NotificationsProperties {
 
     @NotEmpty
     private String notifyDefendantLIPClaimantSettleTheClaimTemplate;
-
     @NotEmpty
     private String hearingNotificationLipDefendantTemplate;
 
