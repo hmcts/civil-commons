1aa2ffb2f053f05468f479d1b6e7044a805ee7c5
Ruban <ruban72@hotmail.com>
2024-05-02 09:04:40 +0100

    DTSCCI_202: Add Ignore Unknown and Non Null Properties to Document

--- src/main/java/uk/gov/hmcts/reform/civil/documentmanagement/model/Document.java
+++ src/main/java/uk/gov/hmcts/reform/civil/documentmanagement/model/Document.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.reform.civil.documentmanagement.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder(toBuilder = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Document {
 
     String documentUrl;
