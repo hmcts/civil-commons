548f2417965d70eaa23701488de36ec37c13f6b6
Velican <duzgunce.vd@gmail.com>
2024-05-09 17:01:46 +0100

    clean up and fix checkStyle issues

--- src/test/java/uk/gov/hmcts/reform/civil/utils/CaseDataContentConverterTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/utils/CaseDataContentConverterTest.java
@@ -4,22 +4,38 @@ import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.ccd.model.Address;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CaseDataContentConverterTest {
+
     private static final String CIVIL_CASE_ID = "12341234";
     private static final String EVENT_ID = "1234";
 
     @Test
     void shouldConvertToUpdatedCaseWithoutDeletingNestedData() {
-        Map<String, Object> applicant1 = new HashMap<>(Map.of("firstName", "app1firstname", "lastName", "app1lastname", "email", "app1@test.com"));
-        Map<String, Object> respondent1 = new HashMap<>(Map.of("firstName", "respond1firstname", "lastName", "respond1lastname", "email", "respond1@test.com"));
-        Map<String, Object> originalData = new HashMap<>(Map.of("caseId", CIVIL_CASE_ID, "applicant1", applicant1, "respondent1", respondent1));
+        Address app1Address = Address.builder()
+            .addressLine1("123 Street")
+            .postTown("City")
+            .postCode("12345")
+            .country("Country")
+            .build();
+        Address resp1Address = Address.builder()
+            .addressLine1("456 Street")
+            .postTown("City")
+            .postCode("54321")
+            .country("Country")
+            .build();
+        Map<String, Object> applicant1 =
+            new HashMap<>(Map.of("firstName", "app1firstname", "lastName", "app1lastname", "email", "app1@test.com", "address", app1Address));
+        Map<String, Object> respondent1 =
+            new HashMap<>(Map.of("firstName", "resp1firstname", "lastName", "resp1lastname", "email", "resp1@test.com", "address", resp1Address));
+        Map<String, Object> originalData =
+            new HashMap<>(Map.of("caseId", CIVIL_CASE_ID, "applicant1", applicant1, "respondent1", respondent1));
         CaseDetails caseDetails = CaseDetails.builder().data(originalData).build();
         StartEventResponse startEventResponse = StartEventResponse.builder()
             .eventId(EVENT_ID)
@@ -32,8 +48,16 @@ class CaseDataContentConverterTest {
         Object convertData = converted.getData();
         assertThat(converted).isNotNull();
 
-        Map<String, Object> expectedData = Map.of("caseId", CIVIL_CASE_ID, "applicant1", applicant1, "respondent1", Map.of("firstName", "updatedfirstname", "lastName", "updatedlastname", "email", "respond1@test.com"));
-
+        Map<String, Object> expectedData =
+            Map.of(
+                "caseId", CIVIL_CASE_ID,
+                "applicant1", applicant1,
+                "respondent1",
+                    Map.of(
+                        "firstName", "updatedfirstname",
+                        "lastName", "updatedlastname",
+                        "email", "resp1@test.com",
+                        "address", resp1Address));
         assertThat(convertData).isEqualTo(expectedData);
     }
 }
