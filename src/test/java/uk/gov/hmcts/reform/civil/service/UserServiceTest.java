6513b8b9e8b9713588ff067797466cacc84d55be
EllisD-B <ellis.domengerboyce@hmcts.net>
2024-07-24 11:39:17 +0100

    Add try catch for potential sensitive info leak to mask email

--- src/test/java/uk/gov/hmcts/reform/civil/service/UserServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/UserServiceTest.java
@@ -7,17 +7,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     private static final String SUB = "user-idam@reform.local";
+    private static final String MASKED_SUB = "*********@************";
     private static final String UID = "user-idam-01";
     private static final String NAME = "User IDAM";
     private static final String GIVEN_NAME = "User";
@@ -65,5 +68,30 @@ class UserServiceTest {
         String accessToken = userService.getAccessToken(SUB, PASSWORD);
 
         assertThat(accessToken).isEqualTo(AUTHORISATION);
+    }
+
+    @Test
+    void shouldReturnUserDetails_whenValidAuthTokenAndResponse() {
+        UserDetails expectedUserDetails = UserDetails.builder()
+            .email(SUB)
+            .build();
+        when(idamClient.getUserDetails(AUTHORISATION)).thenReturn(expectedUserDetails);
+
+        var actualUserDetails = userService.getUserDetails(AUTHORISATION);
+        assertThat(expectedUserDetails).isEqualTo(actualUserDetails);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionWithMaskedEmail_whenFailingToParseIdamResponse() {
+        String errorMessage = "Email is " + SUB;
+        when(idamClient.getUserDetails(AUTHORISATION)).thenThrow(new IllegalArgumentException(errorMessage));
+
+        var exception = assertThrows(
+            IllegalArgumentException.class,
+            () -> userService.getUserDetails(AUTHORISATION)
+        );
+
+        assertThat(exception.getMessage()).contains(MASKED_SUB);
+        assertThat(exception.getMessage()).doesNotContain(SUB);
     }
 }
