2ab3c6df95c1859652338b5355bb9e95f0f0467c
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2024-11-06 15:07:01 +0000

    Added new role assignments query call/models and implemented retrieving role assignments, including labels, for a user in RoleAssignmentService

--- src/main/java/uk/gov/hmcts/reform/civil/ras/client/RoleAssignmentsApi.java
+++ src/main/java/uk/gov/hmcts/reform/civil/ras/client/RoleAssignmentsApi.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import uk.gov.hmcts.reform.civil.ras.model.QueryRequest;
 import uk.gov.hmcts.reform.civil.ras.model.RoleAssignmentRequest;
 import uk.gov.hmcts.reform.civil.ras.model.RoleAssignmentServiceResponse;
 import uk.gov.hmcts.reform.civil.ras.model.UpdateRoleAssignmentResponse;
@@ -29,6 +31,23 @@ public interface RoleAssignmentsApi {
         @RequestHeader(AUTHORIZATION) String authorization,
         @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
         @PathVariable(ACTOR_ID) String actorId);
+
+    @PostMapping(
+        value = "/am/role-assignments/query",
+        consumes = APPLICATION_JSON_VALUE,
+        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
+    )
+    RoleAssignmentServiceResponse getRoleAssignments(
+        @RequestHeader(AUTHORIZATION) String authorization,
+        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+        @RequestHeader(value = "x-correlation-id",
+            required = false) String correlationId,
+        @RequestHeader(value = "pageNumber", required = false) Integer pageNumber,
+        @RequestHeader(value = "size", required = false) Integer size,
+        @RequestHeader(value = "sort", required = false) String sort,
+        @RequestHeader(value = "direction", required = false) String direction,
+        @RequestBody(required = true) QueryRequest queryRequest,
+        @RequestParam(value = "includeLabels", defaultValue = "false") Boolean includeLabels);
 
     @PostMapping(
         value = "/am/role-assignments",
