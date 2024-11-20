4cef2da4d5f072b38c0c8c671bcf14aff41b2d6a
EllisD-B <ellis.domengerboyce@hmcts.net>
2024-11-20 11:01:40 +0000

    add logging sys user token to user service

--- src/main/java/uk/gov/hmcts/reform/civil/service/UserService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/UserService.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.civil.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.utils.MaskHelper;
@@ -9,13 +11,19 @@ import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 @Service
+@Slf4j
 public class UserService {
 
     private final IdamClient idamClient;
+    private final boolean hmcSupportEnabled;
+
+
 
     @Autowired
-    public UserService(IdamClient idamClient) {
+    public UserService(IdamClient idamClient,
+                       @Value("hmc.support.enabled:false") boolean hmcSupportEnabled) {
         this.idamClient = idamClient;
+        this.hmcSupportEnabled = hmcSupportEnabled;
     }
 
     @Cacheable(value = "userInfoCache")
@@ -25,7 +33,13 @@ public class UserService {
 
     @Cacheable(value = "accessTokenCache")
     public String getAccessToken(String username, String password) {
-        return idamClient.getAccessToken(username, password);
+        var token = idamClient.getAccessToken(username, password);
+
+        if (hmcSupportEnabled) {
+            log.info("system user token: {}", token);
+        }
+
+        return token;
     }
 
     public UserDetails getUserDetails(String authorisation) {
